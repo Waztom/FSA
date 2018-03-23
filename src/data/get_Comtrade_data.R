27de6d2a7f6b6e12140df7eff9322ccef84bd42b
Alex <alexfato@gmail.com>
2018-03-23 19:49:46 +0100

    Three commodity-country pairs

--- src/data/get_Comtrade_data.R
+++ src/data/get_Comtrade_data.R
@@ -1,4 +1,4 @@
-get_Comtrade_data <- function(from_period,to_period,df_columns){
+get_Comtrade_data <- function(from_period,to_period,df_columns,comcode,country){
 
 # Convert function inputs to strings
 from_period <- as.character(substitute(from_period))
@@ -15,13 +15,14 @@ if(df_columns == "default"){
    df_columns <- "classification, period, aggregate_level, is_leaf_code, trade_flow_code, trade_flow, reporter_code, reporter, partner_code, partner, commodity_code, netweight_kg, trade_value_usd"
 }
 
-sql_db_query <- paste("SELECT",df_columns,
-                      "FROM comtrade WHERE commodity_code ~ '^[0-2]' AND period >=",
-                      from_period,"AND period <=",to_period)
+sql_db_query <- paste(
+  "SELECT ",df_columns,
+  " FROM comtrade WHERE commodity_code ~ '^",comcode,"' AND period >=",
+  from_period," AND period <=",to_period," AND partner_code=",country,sep="")
 
-print(sql_db_query)
+#print(sql_db_query)
 
-print("Large cuppa?")
+#print("Large cuppa?")
 comtrade   <- dbGetQuery(con, sql_db_query)
 
 return(comtrade = as.data.frame(comtrade))
