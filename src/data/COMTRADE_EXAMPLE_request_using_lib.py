0db068b9f6497d8e656be79ce7af73203556b86f
Janis Hagelberg <astro.janis@gmail.com>
2018-03-27 15:14:15 +0200

    Adding SQL request library and an example script to use it.

---
+++ src/data/COMTRADE_EXAMPLE_request_using_lib.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 27 11:46:43 2018
+
+@author: janis
+"""
+
+import os
+os.sys.path.append('lib')
+import pysqlib
+
+trade=pysqlib.comtrade_sql_request()
