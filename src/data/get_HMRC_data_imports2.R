5f2f4800e7888b00ac26df9801d2b07a498067f1
Alex <alexfato@gmail.com>
2018-03-29 11:28:59 +0200

    Code for comparsion of Comtrade and HMRC has been updated

---
+++ src/data/get_HMRC_data_imports2.R
@@ -0,0 +1,24 @@
+get_HMRC_data_imports2 <- function(df_name,comcode){
+
+# Convert function inputs to strings
+df_name <- as.character(substitute(df_name))
+
+drv <- dbDriver("PostgreSQL")
+con <- dbConnect(drv,
+                 dbname = "hmrc",
+                 host = "data-science-pgsql-dev-01.c8kuuajkqmsb.eu-west-2.rds.amazonaws.com",
+                 user = "trade_read",
+                 password = "2fs@9!^43g")
+
+sql_db_query <- paste("SELECT * FROM ",df_name," WHERE comcode ~ '^",comcode,"'",sep='')
+print("Medium cuppa?")
+requested_df   <- dbGetQuery(con, sql_db_query)
+
+print((paste("SELECT * FROM ",df_name," WHERE comcode ~ '^",comcode,"'",sep='')))
+
+return(requested_df = as.data.frame(requested_df))
+
+on.exit(dbDisconnect(con))
+on.exit(dbUnloadDriver(drv))
+}
+
