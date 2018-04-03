f2d4ec8b11d7aa0b49b8ed3d7286cd10fd0b5130
Janis Hagelberg <astro.janis@gmail.com>
2018-04-03 16:36:41 +0200

    Cleaned up scrape_rasff.py

--- src/data/scrape_rasff.py
+++ src/data/scrape_rasff.py
@@ -1,14 +1,16 @@
-import pandas as pd
-import re
-import time
-import selenium
+#import pandas as pd
+#import re
+#import time
+#import selenium
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+#from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support import expected_conditions as EC
+
 from bs4 import BeautifulSoup
 
+    
 urlbase = "https://webgate.ec.europa.eu/rasff-window/portal/?event=notificationsList&StartRow="
 
 driver = webdriver.Firefox()
