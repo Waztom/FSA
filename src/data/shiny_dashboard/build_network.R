8f5acab08c9a42d128d320a1a137e1380c391c06
Alex Fabregat <fabregat.alex@gmail.com>
2018-04-14 16:30:36 +0200

    Aesthetic changes 2

--- src/data/shiny_dashboard/build_network.R
+++ src/data/shiny_dashboard/build_network.R
@@ -10,9 +10,11 @@ si1 <- si %>% filter(period == all_months[date1])
 total_exports <- si1 %>% group_by (origin) %>% summarize(total_export = sum(trade_value_usd)) %>% rename(label = origin)
 total_imports <- si1 %>% group_by (destin) %>% summarize(total_import = sum(trade_value_usd)) %>% rename(label = destin)
 tmp <- full_join(total_imports, total_exports, by = "label") %>% replace_na(list(total_export=0, total_import=0))
-tmp <- tmp %>%   mutate(a_ratio = (total_import - total_export)/(total_import+total_export)) %>%
-  mutate(value   = log10(total_import+total_export)) %>%
-  mutate(shape   = ifelse(a_ratio > 0.33, "dot", ifelse(a_ratio < -0.33, "triangle", "square")))
+tmp <- tmp %>%   mutate(overall_flux = total_import + total_export) %>% 
+                 mutate(a_ratio = (total_import - total_export)/overall_flux) %>%
+#                 mutate(value   = log10(total_import+total_export)) %>%
+                 mutate(value = 10 * (overall_flux - min(overall_flux))/(max(overall_flux)-min(overall_flux)) + 1) %>%         
+                 mutate(shape   = ifelse(a_ratio > 0.33, "dot", ifelse(a_ratio < -0.33, "triangle", "square")))
 
 #Now the selection!!
 tmp <- tmp %>% filter(quantile(total_export + total_import, threshold) < total_export + total_import)
