811972e3c505d3d29c411bde95b629e0efb836e9
Alex <alexfato@gmail.com>
2018-04-14 18:04:24 +0200

    anomaly detection in shiny app works

--- src/data/shiny_dashboard/anomaly_detection.R
+++ src/data/shiny_dashboard/anomaly_detection.R
@@ -12,6 +12,6 @@ df2_anomalies <- df2_analysed %>% filter(anomaly == "Yes")
 anomaly_plot <- df2_analysed %>% time_recompose() %>%
                 plot_anomalies(time_recomposed = TRUE, ncol=3, color_no = "purple", color_yes = "orange", alpha_ribbon = 1)
 
-return(ad_plot)
+return(anomaly_plot)
 
 }
