27d3abaf923e3c59124441d4963b1f61b5a5c45a
Alex <alexfato@gmail.com>
2018-04-16 12:20:29 +0200

    some changes

--- src/data/shiny_dashboard/model_get_data.R
+++ src/data/shiny_dashboard/model_get_data.R
@@ -1,5 +1,24 @@
-model_get_data <- function(csv_file_name){
+model_get_data <- function(commodity){
 
+#Set the path
+  pathname <- "../../../data/"
+
+#Choose the commodity in a crappy way
+if(commodity == "Cucumbers"){
+  comname = "comtrade_all_cucumber.csv"
+}else if(commodity == "Vanilla"){
+  comname = "comtrade_all_vanilla.csv"
+}else if(commodity == "Beer"){
+  comname = "comtrade_all_beer.csv"
+}else if(commodity == "Milk"){
+  comname = "comtrade_all_milk.csv"
+}else if(commodity == "Maple Syrup"){
+  comname = "comtrade_all_maplesyrup.csv"
+}
+  
+#Build the filename
+  csv_file_name <- paste(pathname,comname)
+  
 # Read the csv file for a commodity
 df <- read.csv(file=csv_file_name, header=TRUE)
 
