55f51c422034074d72c2e4a2280c00ed0437c58a
Waztom <waztom@gmail.com>
2018-04-16 15:50:16 +0100

    #

--- src/models/dynamic_network_modelling_btergm/get_Comtrade_monthly_data.R
+++ src/models/dynamic_network_modelling_btergm/get_Comtrade_monthly_data.R
@@ -3,14 +3,10 @@ get_Comtrade_monthly_data <- function(df){
   df <- df %>% filter(trade_flow != "Re-imports" & trade_flow != "Re-exports" & reporter != "World" & reporter != "EU-27" & partner != "World" & partner != "EU-27")
   
   months <- df %>% select(period) %>% unique() %>% arrange(period)
-  data <- df
   listofdfs <- list()
-  intial <- 1
   
-  for (i in 1:(nrow(months)-1)){
-    time_start <- months$period[i]
-    time_end <- months$period[intial+i]
-    month <- data %>% filter(period < time_end & period >= time_start) %>% select(period,trade_flow,reporter,partner,netweight_kg,trade_value_usd)  
+  for (i in 1:nrow(months)){
+    month <- df %>% filter(period == months[[1]][i]) %>% select(period,trade_flow,reporter,partner,netweight_kg,trade_value_usd)
     listofdfs[[i]] <- month
   }
   return(listofdfs)
