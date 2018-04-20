9fbed220de4ecb9a64b3540a75e0233324aaa87d
Phoebe1992 <phoebe.macdonald@bridge.local>
2018-04-20 12:24:10 +0100

    flipping axis to aid interpretation

--- handover/shiny_dashboard/anomaly_detection.R
+++ handover/shiny_dashboard/anomaly_detection.R
@@ -11,7 +11,7 @@ df2_anomalies <- df2_analysed %>% filter(anomaly == "Yes")
 #Recomposing time
 anomaly_recomposed <- df2_analysed %>% time_recompose()
 #Create graph with anomalies detected
-anomaly_plot <- plot_anomalies(anomaly_recomposed, time_recomposed = TRUE, ncol=3, color_no = "darkgreen", color_yes = "green", alpha_ribbon = 1) + ylim(c(-1 ,1 )) + labs(labs(x="Time", y="Producer                                      Distributor                                      Consumer")) + theme(axis.text=element_text(size=12),
+anomaly_plot <- plot_anomalies(anomaly_recomposed, time_recomposed = TRUE, ncol=3, color_no = "darkgreen", color_yes = "green", alpha_ribbon = 1) + ylim(c(1 ,-1 )) + labs(labs(x="Time", y="Consumer                                      Distributor                                      Producer")) + theme(axis.text=element_text(size=12),
                                                                                                                                                                                                                                                                     axis.title=element_text(size=14,face="bold"), legend.text=element_text(size=14), legend.title=element_text(size=14))
 
 return(anomaly_plot)
